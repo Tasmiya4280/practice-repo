6241ceadcfd0666c52a153e6923fa982529237e4
Tasmiya4280 <138672611+Tasmiya4280@users.noreply.github.com>
2025-01-23 23:05:13 +0500

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -5,3 +5,18 @@ client = boto3.client('s3')
 response = client.create_bucket(
     Bucket = 'abc',
 )
+
+
+def create_ec2_instance():
+    ec2 = boto3.client('ec2', region_name='us-east-1')  
+
+    response = ec2.run_instances(
+        ImageId='ami-0c2b8ca1dad447f8a', 
+        InstanceType='t2.micro',  
+        KeyName='xyz', 
+    )
+
+    instance_id = response['Instances'][0]['InstanceId']
+    print(f"Instance created with ID: {instance_id}")
+    
+create_ec2_instance()
