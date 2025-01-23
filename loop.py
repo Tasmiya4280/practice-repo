fd3eb473d03105202b2be12bba5f42398c1694fc
Tasmiya4280 <138672611+Tasmiya4280@users.noreply.github.com>
2025-01-23 23:14:29 +0500

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,13 +1,19 @@
-list_of_clouds= ["aws", "azure", "gcp"]
+list_of_clouds = ["aws", "azure", "gcp"]
 print(list_of_clouds)
 
 list_of_clouds.append("linode")
-list_of_clouds.insert(0 ,"digital ocean")
+list_of_clouds.insert(0, "digital ocean")
 print(list_of_clouds)
-print (len(list_of_clouds))
+print(len(list_of_clouds))
 
 for cloud in list_of_clouds:
- print(" ")
- print(cloud)
-for i in range(1,5):
- print(list_of_clouds)
+    print(" ")
+    print(cloud)
+
+counter = 1
+while counter <= 5:
+    print(f"Counter: {counter}")
+    counter += 1
+print("Loop finished!")
+ 
+
